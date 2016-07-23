9250e06f4b6e5b242c48d26c97a49745ea953d6c
Ana Gomes <Ana Gomes>
2016-07-23 00:28:26 -0300

    Testing swwthome2d project

--- src/main/java/br/ufal/ic/extract/minerator/MyStudy.java
+++ src/main/java/br/ufal/ic/extract/minerator/MyStudy.java
@@ -1,7 +1,5 @@
 package br.ufal.ic.extract.minerator;
 
-import org.apache.log4j.Logger;
-
 import br.com.metricminer2.MetricMiner2;
 import br.com.metricminer2.RepositoryMining;
 import br.com.metricminer2.Study;
@@ -11,17 +9,17 @@ import br.com.metricminer2.scm.commitrange.Commits;
 
 public class MyStudy implements Study{
 	
-	static Logger log = Logger.getLogger(MyStudy.class);
-		public static void main(String[] args) {
-			new MetricMiner2().start(new MyStudy());
-		}
-		
-		public void execute() {
-			new RepositoryMining()
-			.in(GitRepository.singleProject("C:\\Users\\Ana Carla\\ProjetosAnalisados\\SweetHome3D\\"))
-			.through(Commits.all())
-			.process(new DevelopersVisitor(), new CSVFile("C:\\Users\\Ana Carla\\ProjetosAnalisados\\TestLog4jHistoric.csv"))
-			.mine();
-		}
+	
+	public static void main(String[] args) {
+		new MetricMiner2().start(new MyStudy());
+	}
+	
+	public void execute() {
+		new RepositoryMining()
+		.in(GitRepository.singleProject("C:\\Users\\Ana Carla\\ProjetosAnalisados\\sweethome3d"))
+		.through(Commits.all())
+		.process(new DevelopersVisitor(), new CSVFile("C:\\Users\\Ana Carla\\ProjetosAnalisados\\TestLog4jHistoric.csv"))
+		.mine();
+	}
 	
 }
